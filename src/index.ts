6a60ec2ebcd67a8e86ae18fadab0cd7a0c1a6144
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2021-09-04 22:03:28 -0700

    final commit

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,35 @@
-import { handleRequest } from './handler'
+import { Router } from 'itty-router'
+import bcrypt from 'bcryptjs'
 
-addEventListener('fetch', (event) => {
-  event.respondWith(handleRequest(event.request))
+const router = Router()
+
+const iterations = 12
+
+router.get('/hash', ({ query }) => {
+  try {
+    const password = decodeURIComponent(query?.password ?? '')
+    if (!password)
+      return new Response('Missing password or hash', { status: 400 })
+    const hash = bcrypt.hashSync(password, iterations)
+    return new Response(hash, { status: 200 })
+  } catch (error: any) {
+    return new Response(error.message, { status: 500 })
+  }
+})
+
+router.get('/compare', ({ query }) => {
+  const password = decodeURIComponent(query?.password ?? '')
+  const hash = decodeURIComponent(query?.hash ?? '')
+  if (!password || !hash)
+    return new Response('Missing password or hash', { status: 400 })
+  const areEqual = bcrypt.compareSync(password, hash)
+  return new Response(areEqual ? 'success' : 'failure', {
+    status: areEqual ? 200 : 400,
+  })
+})
+
+router.all('*', () => new Response('404, not found!', { status: 404 }))
+
+addEventListener('fetch', (e) => {
+  e.respondWith(router.handle(e.request))
 })
